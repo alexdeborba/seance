72a676f59e42f71358e40826225cf8d419f78b8e
Atmostfear Entertainment <mail@alexdeborba.com>
2023-07-02 10:35:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-// There is nothing output here because Block Themes such as Séance do not use PHP templates.
+/**
+ * There is nothing output here because Block Themes such as Séance do not use PHP templates.
+ *
+ * @package seance
+ *
+ * There is a core ticket discussing removing this requirement for block themes:
+ * https://core.trac.wordpress.org/ticket/54272.
+ */
