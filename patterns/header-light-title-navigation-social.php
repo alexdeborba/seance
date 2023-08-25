85c0941b15064e1adaeff76a742a70feac2c8ec5
Atmostfear Entertainment <mail@alexdeborba.com>
2023-08-25 17:39:54 -0500

    Create header-light-title-navigation-social.php

---
+++ patterns/header-light-title-navigation-social.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: Header with site title, navigation, and social icons on light background
+ * Slug: seance/header-light-title-navigation-social
+ * Categories: header
+ * Description: Header with site title, navigation, and social icons on light background block pattern
+ * Keywords: header, navigation, social
+ * Viewport Width: 1500
+ * Block Types: core/template-part/header
+ * Post Types: wp_template
+ * Inserter: true
+ * 
+ * @package seance
+ * @since 1.0.0
+ */
+?>
+
+<!-- wp:group {"align":"full","layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull">
+    <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var(--wp--custom--spacing--large, 8rem)","top":"var(--wp--custom--spacing--small, 1.25rem)"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+    <div class="wp-block-group alignwide" style="padding-top:var(--wp--custom--spacing--small, 1.25rem);padding-bottom:var(--wp--custom--spacing--large, 8rem)">
+        <!-- wp:site-title {"style":{"typography":{"fontStyle":"italic","fontWeight":"400"}}} /-->
+        <!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} -->
+        <!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
+        <!-- wp:social-links {"iconColor":"foreground","iconColorValue":"var(--wp--preset--color--foreground)","className":"is-style-logos-only"} -->
+        <ul class="wp-block-social-links has-icon-color is-style-logos-only">
+            <!-- wp:social-link {"url":"#","service":"facebook"} /-->
+            <!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+            <!-- wp:social-link {"url":"#","service":"twitter"} /-->
+            <!-- wp:social-link {"url":"#","service":"instagram"} /-->
+            <!-- wp:social-link {"url":"#","service":"pinterest"} /-->
+            <!-- wp:social-link {"url":"#","service":"wordpress"} /-->
+        </ul>
+        <!-- /wp:social-links -->
+        <!-- /wp:navigation -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
