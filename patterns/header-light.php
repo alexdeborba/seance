e560f277635a457e7158c9f801435792c941f7f6
Atmostfear Entertainment <mail@alexdeborba.com>
2023-08-25 17:02:16 -0500

    Create header-light.php

---
+++ patterns/header-light.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: Header with site logo and site title on the left, and navigation on the right
+ * Slug: seance/header-light
+ * Categories: header
+ * Description: Header with title and navigation on a dark block pattern
+ * Keywords: header, logo, navigation
+ * Viewport Width: 1500
+ * Block Types: core/template-part/header
+ * Post Types: wp_template
+ * Inserter: true
+ * 
+ * @package seance
+ * @since 1.0.0
+ */
+?>
+
+<!-- wp:group {"align":"full","layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var(--wp--custom--spacing--large, 8rem)","top":"var(--wp--custom--spacing--small, 1.25rem)"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<div class="wp-block-group alignwide" style="padding-top:var(--wp--custom--spacing--small, 1.25rem);padding-bottom:var(--wp--custom--spacing--large, 8rem)">
+		<!-- wp:group {"layout":{"type":"flex"}} -->
+		<div class="wp-block-group">
+			<!-- wp:site-logo {"width":64} /-->
+			<!-- wp:site-title {"style":{"typography":{"fontStyle":"italic","fontWeight":"400"}}} /-->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} -->
+		<!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
+		<!-- /wp:navigation -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
