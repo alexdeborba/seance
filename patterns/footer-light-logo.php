526a229dd9e1931761bb879e568ac52188800ca8
Atmostfear Entertainment <mail@alexdeborba.com>
2023-08-25 13:24:06 -0500

    Create footer-light-logo.php

---
+++ patterns/footer-light-logo.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Title: Footer Light with site logo, and copyright
+ * Slug: seance/footer-light-logo
+ * Categories: footer
+ * Description: Footer with logo and copyright in a light block pattern
+ * Keywords: footer
+ * Viewport Width: 1500
+ * Block Types: core/template-part/footer
+ * Post Types: wp_template
+ * Inserter: true
+ * 
+ * @package seance
+ * @since 1.0.0
+ */
+?>
+
+<!-- wp:group {"align":"full","layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull">
+    <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"4rem","bottom":"4rem"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+    <div class="wp-block-group alignwide" style="padding-top:4rem;padding-bottom:4rem"><!-- wp:site-logo {"width":60} /-->
+        <!-- wp:paragraph {"align":"right"} -->
+        <p class="has-text-align-right">
+            <?php
+            /* Translators: WordPress link. */
+            $wordpress_link = '<a href="' . esc_url(__('https://wordpress.org', 'twentytwentyfour')) . '" rel="nofollow">WordPress</a>';
+            echo sprintf(
+                esc_html__('Proudly powered by %1$s', 'seance'),
+                $wordpress_link
+            );
+            ?>
+        </p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
