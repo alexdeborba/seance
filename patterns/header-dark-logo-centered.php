103ac105c40685677fc4e7ffd36070a1c2ced070
Atmostfear Entertainment <mail@alexdeborba.com>
2023-08-25 16:15:32 -0500

    Create header-dark-logo-centered.php

---
+++ patterns/header-dark-logo-centered.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: Header with site title on the left, centered logo, navigation on the right
+ * Slug: seance/header-dark-logo-centered
+ * Categories: header
+ * Description: Header with centered logo on a dark block pattern
+ * Keywords: header, logo, navigation
+ * Viewport Width: 1500
+ * Block Types: core/template-part/header
+ * Post Types: wp_template
+ * Inserter: true
+ * 
+ * @package seance
+ * @since 1.0.0
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"var(--wp--custom--spacing--small, 1.25rem)","bottom":"var(--wp--custom--spacing--small, 1.25rem)"}}},"backgroundColor":"foreground","textColor":"background","layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull has-background-color has-foreground-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--custom--spacing--small, 1.25rem);padding-bottom:var(--wp--custom--spacing--small, 1.25rem);">
+    <!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+    <div class="wp-block-columns alignwide are-vertically-aligned-center">
+        <!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center">
+            <!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"400","textTransform":"uppercase"}}} /-->
+        </div>
+        <!-- /wp:column -->
+        <!-- wp:column {"width":"64px"} -->
+        <div class="wp-block-column" style="flex-basis:64px">
+            <!-- wp:site-logo {"width":64} /-->
+        </div>
+        <!-- /wp:column -->
+        <!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center">
+            <!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} -->
+            <!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
+            <!-- /wp:navigation -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
