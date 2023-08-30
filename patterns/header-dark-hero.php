2202a54e297a5db2f462704b2ecabb582d803f66
Atmostfear Entertainment <mail@alexdeborba.com>
2023-08-30 14:22:00 -0500

    Update header-dark-hero.php

--- patterns/header-dark-hero.php
+++ patterns/header-dark-hero.php
@@ -19,7 +19,7 @@
 <div class="wp-block-cover has-primary-color has-text-color" style="margin-top:0px;margin-bottom:0px;padding-top:0;padding-bottom:0">
     <span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(180deg,rgba(14,14,15,0.67) 0%,rgb(15,15,16) 47%)"></span><img class="wp-block-cover__image-background wp-image-1747" alt="<?php echo esc_html_e('The Fox Sisters', 'seance'); ?>" src="<?php echo esc_url(get_theme_file_uri("/assets/images/header-dark-hero.jpg")); ?>" data-object-fit="cover" />
     <div class="wp-block-cover__inner-container">
-        <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dspacing\u002d\u002dsmall, 1.25rem)","bottom":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dspacing\u002d\u002dsmall, 1.25rem)"}}},"layout":{"type":"constrained"}} -->
+        <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var(--wp--custom--spacing--small, 1.25rem)","bottom":"var(--wp--custom--spacing--small, 1.25rem)"}}},"layout":{"type":"constrained"}} -->
         <div class="wp-block-group alignfull" style="padding-top:var(--wp--custom--spacing--small, 1.25rem);padding-bottom:var(--wp--custom--spacing--small, 1.25rem)">
             <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"0rem","top":"0px","right":"0px","left":"0px"}}},"layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
             <div class="wp-block-group alignwide" style="padding-top:0px;padding-right:0px;padding-bottom:0rem;padding-left:0px">
@@ -27,7 +27,7 @@
                 <!-- wp:navigation {"ref":1735,"overlayBackgroundColor":"foreground","overlayTextColor":"primary","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
             </div>
             <!-- /wp:group -->
-            <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dspacing\u002d\u002dsmall, 1.25rem)","bottom":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dspacing\u002d\u002dlarge, 8rem)"}}},"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal"}} -->
+            <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var(--wp--custom--spacing--small, 1.25rem)","bottom":"var(--wp--custom--spacing--large, 8rem)"}}},"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal"}} -->
             <div class="wp-block-group alignwide" style="padding-top:var(--wp--custom--spacing--small, 1.25rem);padding-bottom:var(--wp--custom--spacing--large, 8rem)">
                 <!-- wp:spacer {"width":"0px","style":{"layout":{"flexSize":"60px","selfStretch":"fixed"}}} -->
                 <div style="height:100px;width:0px" aria-hidden="true" class="wp-block-spacer"></div>
