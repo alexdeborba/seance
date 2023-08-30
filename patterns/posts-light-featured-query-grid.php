ffd1e2659944e16819c6f5b077e9f2255e16c4b8
Atmostfear Entertainment <mail@alexdeborba.com>
2023-08-30 15:53:54 -0500

    Update posts-light-featured-query-grid.php

--- patterns/posts-light-featured-query-grid.php
+++ patterns/posts-light-featured-query-grid.php
@@ -16,11 +16,13 @@
 ?>
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var(--wp--custom--spacing--large, 8rem)","bottom":"var(--wp--custom--spacing--large, 8rem)"}}},"layout":{"inherit":false}} -->
-<div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}}} -->
+<div class="wp-block-group alignwide">
+    <!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}}} -->
     <p style="text-transform:uppercase"><?php echo esc_html_e('Latest Posts', 'seance'); ?></p>
     <!-- /wp:paragraph -->
     <!-- wp:query {"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"displayLayout":{"type":"flex","columns":3}} -->
-    <div class="wp-block-query"><!-- wp:post-template -->
+    <div class="wp-block-query">
+        <!-- wp:post-template -->
         <!-- wp:post-featured-image {"isLink":true,"width":"","height":"310px"} /-->
         <!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
         <!-- wp:post-excerpt /-->
