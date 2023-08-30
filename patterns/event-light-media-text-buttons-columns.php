a7543738bc0df19efef65de841b7b82b3a6c13a0
Atmostfear Entertainment <mail@alexdeborba.com>
2023-08-30 10:10:46 -0500

    Update  event-light-media-text-buttons-columns.php

--- patterns/event-light-media-text-buttons-columns.php
+++ patterns/event-light-media-text-buttons-columns.php
@@ -18,20 +18,19 @@
 <!-- wp:group {"align":"wide"} -->
 <div class="wp-block-group alignwide">
     <!-- wp:image {"sizeSlug":"large"} -->
-    <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/welcome-unknown.jpg")); ?>" alt="<?php echo esc_html_e('The Unknown Widows', 'seance'); ?>" /></figure>
+    <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/welcome-unknown.jpg")); ?>" alt="<?php echo esc_html_e('The Fox Sisters', 'seance'); ?>" /></figure>
     <!-- /wp:image -->
     <!-- wp:columns {"verticalAlignment":null} -->
-    <div class="wp-block-columns">
-        <!-- wp:column {"verticalAlignment":"bottom"} -->
-        <div class="wp-block-column is-vertically-aligned-bottom">
+    <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top"} -->
+        <div class="wp-block-column is-vertically-aligned-top">
             <!-- wp:heading {"style":{"typography":{"fontSize":"clamp(3.25rem, 8vw, 6.25rem)","lineHeight":"1.15"}}} -->
-            <h2 style="font-size:clamp(3.25rem, 8vw, 6.25rem);line-height:1.15"><?php echo wp_kses_post(__('Welcome to<br>the <em>Unknown</em>', 'seance')); ?></h2>
+            <h2 class="wp-block-heading" style="font-size:clamp(3.25rem, 8vw, 6.25rem);line-height:1.15"><?php echo wp_kses_post(__('Welcome to<br>the <em>Unknown</em>', 'seance')); ?></h2>
             <!-- /wp:heading -->
         </div>
         <!-- /wp:column -->
         <!-- wp:column {"verticalAlignment":"bottom","style":{"spacing":{"padding":{"bottom":"6rem"}}}} -->
         <div class="wp-block-column is-vertically-aligned-bottom" style="padding-bottom:6rem">
-        <!-- wp:paragraph -->
+            <!-- wp:paragraph -->
             <p><?php echo wp_kses_post(__('From the pioneers of Spiritualism, now in the digital realm, experience a séance like no other. Secure your spot for this exclusive online event and connect with the beyond. Tickets are limited.<br><br>Do not miss your chance to bridge the worlds.', 'seance')); ?></p>
             <!-- /wp:paragraph -->
             <!-- wp:spacer {"height":20} -->
