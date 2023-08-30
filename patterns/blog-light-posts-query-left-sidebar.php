417e5cad8d2a82f2f61702421957738ac1f25f31
Atmostfear Entertainment <mail@alexdeborba.com>
2023-08-30 09:16:01 -0500

    Update blog-light-posts-query-left-sidebar.php

--- patterns/blog-light-posts-query-left-sidebar.php
+++ patterns/blog-light-posts-query-left-sidebar.php
@@ -3,7 +3,7 @@
  * Title: Blog light posts with heading, title, featured image, excerpt, and date with left sidebar 
  * Slug: seance/blog-light-posts-query-left-sidebar
  * Categories: pages
- * Description: Blog posts with left sidebar in a light block pattern
+ * Description: Blog posts with left sidebar on a light block pattern
  * Keywords: page, query, blog, posts, sidebar
  * Viewport Width: 1500
  * Block Types: core/pages
