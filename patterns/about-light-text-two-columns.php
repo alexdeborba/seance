885991ba74d524296f189778c91d080e792e22bb
Atmostfear Entertainment <mail@alexdeborba.com>
2023-08-31 17:43:45 -0500

    Update about-light-text-two-columns.php

--- patterns/about-light-text-two-columns.php
+++ patterns/about-light-text-two-columns.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: About light with heading, paragraphs on two columns
- * Slug: seance/about-light-text-two-columns
+ * Slug: seancewp/about-light-text-two-columns
  * Categories: pages
  * Description: About with text on two columns on a light block pattern
  * Keywords: heading, paragraph, columns
@@ -10,7 +10,7 @@
  * Post Types:
  * Inserter: true
  * 
- * @package seance
+ * @package seancewp
  * @since 1.0.0
  */
 ?>
@@ -18,7 +18,7 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var(--wp--custom--spacing--large, 8rem)","bottom":"var(--wp--custom--spacing--large, 8rem)"}}},"layout":{"inherit":true}} -->
 <div class="wp-block-group alignfull" style="padding-top:var(--wp--custom--spacing--large, 8rem);padding-bottom:var(--wp--custom--spacing--large, 8rem);">
     <!-- wp:heading {"level":1,"align":"wide","style":{"typography":{"fontSize":"clamp(4rem, 15vw, 12.5rem)","lineHeight":"1","fontWeight":"200"}}} -->
-    <h1 class="alignwide" style="font-size:clamp(4rem, 15vw, 12.5rem);font-weight:200;line-height:1"><?php echo wp_kses_post(__('<em>Life </em><br><em>&amp; Death</em>', 'seance')); ?></h1>
+    <h1 class="alignwide" style="font-size:clamp(4rem, 15vw, 12.5rem);font-weight:200;line-height:1"><?php echo wp_kses_post(__('<em>Life </em><br><em>&amp; Death</em>', 'seancewp')); ?></h1>
     <!-- /wp:heading -->
     <!-- wp:spacer {"height":50} -->
     <div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
@@ -30,7 +30,7 @@
             <!-- wp:column {"verticalAlignment":"center","width":"20%"} -->
             <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20%">
                 <!-- wp:paragraph -->
-                <p><?php echo wp_kses_post(__('Greetings', 'seance')); ?></p>
+                <p><?php echo wp_kses_post(__('Greetings', 'seancewp')); ?></p>
                 <!-- /wp:paragraph -->
             </div>
             <!-- /wp:column -->
@@ -50,7 +50,7 @@
         <!-- wp:column -->
         <div class="wp-block-column">
             <!-- wp:paragraph -->
-            <p><?php echo wp_kses_post(__('We are the Fox sisters, Margaret, Catherine, and Leah.<br><br>We have been blessed with the ability to communicate with the spirits, and through us, many have found answers and solace from the other side.<br><br>We first discovered our gift in our home in <a href="#">Hydesville</a>, <a href="#">New York</a>, where we communicated with a spirit through mysterious rappings.<br><br>Our experiences have not only transformed our lives but have also ignited a movement, bringing hope and understanding to many about the mysteries of the afterlife.<br><br>As pioneers in the Spiritualist movement, we have traveled extensively, sharing our abilities and stories with those eager to connect with the world beyond.<br><br>Our journey has been both enlightening and challenging, as we have encountered both believers and skeptics. Yet, our mission remains steadfast: to bridge the gap between the living and the departed, offering a glimpse into the eternal.<br><br>May the spirits guide and protect you until we meet again.<br><br>Farewell.', 'seance')) ?></p>
+            <p><?php echo wp_kses_post(__('We are the Fox sisters, Margaret, Catherine, and Leah.<br><br>We have been blessed with the ability to communicate with the spirits, and through us, many have found answers and solace from the other side.<br><br>We first discovered our gift in our home in <a href="#">Hydesville</a>, <a href="#">New York</a>, where we communicated with a spirit through mysterious rappings.<br><br>Our experiences have not only transformed our lives but have also ignited a movement, bringing hope and understanding to many about the mysteries of the afterlife.<br><br>As pioneers in the Spiritualist movement, we have traveled extensively, sharing our abilities and stories with those eager to connect with the world beyond.<br><br>Our journey has been both enlightening and challenging, as we have encountered both believers and skeptics. Yet, our mission remains steadfast: to bridge the gap between the living and the departed, offering a glimpse into the eternal.<br><br>May the spirits guide and protect you until we meet again.<br><br>Farewell.', 'seancewp')) ?></p>
             <!-- /wp:paragraph -->
         </div>
         <!-- /wp:column -->
