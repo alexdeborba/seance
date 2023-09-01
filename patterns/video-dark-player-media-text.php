532ce919414d627477797634ac0a58801377ef95
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-01 07:04:55 -0500

    Update video-dark-player-media-text.php

--- patterns/video-dark-player-media-text.php
+++ patterns/video-dark-player-media-text.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Video dark player with media, heading, and paragraphs
- * Slug: seance/video-dark-player-media-text
+ * Slug: seancewp/video-dark-player-media-text
  * Categories: media, columns, media, video
  * Description: Video player with header and text in a dark block pattern
  * Keywords: video
@@ -10,7 +10,7 @@
  * Post Types:
  * Inserter: true
  * 
- * @package seance
+ * @package seancewp
  * @since 1.0.0
  */
 ?>
@@ -20,13 +20,13 @@
     <!-- wp:group {"align":"full","layout":{"inherit":true}} -->
     <div class="wp-block-group alignfull">
         <!-- wp:heading {"level":1,"align":"wide","style":{"typography":{"fontSize":"clamp(3rem, 6vw, 4.5rem)"}}} -->
-        <h1 class="alignwide" id="warble-a-film-about-hobbyist-bird-watchers-1" style="font-size:clamp(3rem, 6vw, 4.5rem)"><?php echo wp_kses_post(__('<em>\'Séance,\'</em> a film exploring the<br>enigmatic world of spirits.', 'seance')); ?></h1>
+        <h1 class="alignwide" id="warble-a-film-about-hobbyist-bird-watchers-1" style="font-size:clamp(3rem, 6vw, 4.5rem)"><?php echo wp_kses_post(__('<em>\'Séance,\'</em> a film exploring the<br>enigmatic world of spirits.', 'seancewp')); ?></h1>
         <!-- /wp:heading -->
         <!-- wp:spacer {"height":32} -->
         <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
         <!-- /wp:spacer -->
         <!-- wp:video {"align":"wide"} -->
-        <figure class="wp-block-video alignwide"><video controls src="<?php echo esc_url(get_theme_file_uri("/assets/videos/seance.mp4")); ?>"></video></figure>
+        <figure class="wp-block-video alignwide"><video controls src="<?php echo esc_url(get_theme_file_uri("/assets/videos/seancewp.mp4")); ?>"></video></figure>
         <!-- /wp:video -->
         <!-- wp:spacer {"height":32} -->
         <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
@@ -36,21 +36,21 @@
             <!-- wp:column {"width":"50%"} -->
             <div class="wp-block-column" style="flex-basis:50%">
                 <!-- wp:paragraph -->
-                <p><strong><?php echo esc_html_e('Featuring', 'seance'); ?></strong></p>
+                <p><strong><?php echo esc_html_e('Featuring', 'seancewp'); ?></strong></p>
                 <!-- /wp:paragraph -->
             </div>
             <!-- /wp:column -->
             <!-- wp:column -->
             <div class="wp-block-column">
                 <!-- wp:paragraph -->
-                <p><?php echo wp_kses_post(__('Margaret Fox<br>Catherine Fox<br>Leah Fox<br>Eusapia Palladino<br>Sir Arthur Conan Doyle', 'seance')); ?></p>
+                <p><?php echo wp_kses_post(__('Margaret Fox<br>Catherine Fox<br>Leah Fox<br>Eusapia Palladino<br>Sir Arthur Conan Doyle', 'seancewp')); ?></p>
                 <!-- /wp:paragraph -->
             </div>
             <!-- /wp:column -->
             <!-- wp:column -->
             <div class="wp-block-column">
                 <!-- wp:paragraph -->
-                <p><?php echo wp_kses_post(__('Daniel Dunglas Home<br>Florence Cook<br>William Stainton Moses<br>Helena Blavatsky<br>Leonora Piper', 'seance')); ?></p>
+                <p><?php echo wp_kses_post(__('Daniel Dunglas Home<br>Florence Cook<br>William Stainton Moses<br>Helena Blavatsky<br>Leonora Piper', 'seancewp')); ?></p>
                 <!-- /wp:paragraph -->
             </div>
             <!-- /wp:column -->
