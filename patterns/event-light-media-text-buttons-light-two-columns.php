1dd7b810a8aa1b9af9ebd4b998aef2a3c930b612
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-01 06:12:34 -0500

    Update event-light-media-text-buttons-light-two-columns.php

--- patterns/event-light-media-text-buttons-light-two-columns.php
+++ patterns/event-light-media-text-buttons-light-two-columns.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Event light media with heading, paragraphs, buttons on two columns
- * Slug: seance/event-light-media-text-buttons-light-two-columns
+ * Slug: seancewp/event-light-media-text-buttons-light-two-columns
  * Categories: featured, columns, gallery, image, buttons
  * Description: Event with two images, text and buttons on a light block pattern
  * Keywords: image, heading, paragraph, columns
@@ -10,7 +10,7 @@
  * Post Types:
  * Inserter: true
  * 
- * @package seance
+ * @package seancewp
  * @since 1.0.0
  */
 ?>
@@ -22,16 +22,16 @@
         <!-- wp:column {"style":{"spacing":{"padding":{"top":"8rem","right":"0rem","bottom":"8rem","left":"0rem"}}}} -->
         <div class="wp-block-column" style="padding-top:8rem;padding-right:0rem;padding-bottom:8rem;padding-left:0rem">
             <!-- wp:image {"sizeSlug":"large"} -->
-            <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/poster-light-left.jpg")); ?>" alt="<?php echo esc_html_e('The Fox Sisters', 'seance'); ?>" /></figure>
+            <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/poster-light-left.jpg")); ?>" alt="<?php echo esc_html_e('The Fox Sisters', 'seancewp'); ?>" /></figure>
             <!-- /wp:image -->
             <!-- wp:spacer {"height":30} -->
             <div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->
             <!-- wp:heading {"fontSize":"x-large"} -->
-            <h2 class="has-x-large-font-size" id="spiritual-summit"><?php echo esc_html_e('Spiritual Summit', 'seance'); ?></h2>
+            <h2 class="has-x-large-font-size" id="spiritual-summit"><?php echo esc_html_e('Spiritual Summit', 'seancewp'); ?></h2>
             <!-- /wp:heading -->
             <!-- wp:paragraph -->
-            <p><?php echo wp_kses_post(__('March 31, 1848 @ 7:00PM<br>Hydesville Memorial Park<br>1510 Hydesville Road, Newark<br>NY 14513', 'seance')); ?></p>
+            <p><?php echo wp_kses_post(__('March 31, 1848 @ 7:00PM<br>Hydesville Memorial Park<br>1510 Hydesville Road, Newark<br>NY 14513', 'seancewp')); ?></p>
             <!-- /wp:paragraph -->
             <!-- wp:spacer {"height":8} -->
             <div style="height:8px" aria-hidden="true" class="wp-block-spacer"></div>
@@ -42,7 +42,7 @@
             <!-- wp:buttons -->
             <div class="wp-block-buttons">
                 <!-- wp:button {"backgroundColor":"foreground"} -->
-                <div class="wp-block-button"><a class="wp-block-button__link has-foreground-background-color has-background"><?php echo esc_html_e('Buy Tickets', 'seance'); ?></a></div>
+                <div class="wp-block-button"><a class="wp-block-button__link has-foreground-background-color has-background"><?php echo esc_html_e('Buy Tickets', 'seancewp'); ?></a></div>
                 <!-- /wp:button -->
             </div>
             <!-- /wp:buttons -->
@@ -51,16 +51,16 @@
         <!-- wp:column {"style":{"spacing":{"padding":{"top":"8rem","right":"0rem","bottom":"8rem","left":"0rem"}}}} -->
         <div class="wp-block-column" style="padding-top:8rem;padding-right:0rem;padding-bottom:8rem;padding-left:0rem">
             <!-- wp:image {"sizeSlug":"large"} -->
-            <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/poster-light-right.jpg")); ?>"" alt=" <?php echo esc_html_e('The Fox Sisters', 'seance'); ?>" /></figure>
+            <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/poster-light-right.jpg")); ?>"" alt=" <?php echo esc_html_e('The Fox Sisters', 'seancewp'); ?>" /></figure>
             <!-- /wp:image -->
             <!-- wp:spacer {"height":30} -->
             <div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->
             <!-- wp:heading {"fontSize":"x-large"} -->
-            <h2 class="has-x-large-font-size" id="ethereal-echoes"><?php echo esc_html_e('Ethereal Echoes', 'seance'); ?></h2>
+            <h2 class="has-x-large-font-size" id="ethereal-echoes"><?php echo esc_html_e('Ethereal Echoes', 'seancewp'); ?></h2>
             <!-- /wp:heading -->
             <!-- wp:paragraph -->
-            <p><?php echo wp_kses_post(__('November 14, 1888 @ 7:00PM<br>Cypress Hills Cemetery<br>833 Jamaica Avenue<br>Brooklyn, NY 11208', 'seance')); ?></p>
+            <p><?php echo wp_kses_post(__('November 14, 1888 @ 7:00PM<br>Cypress Hills Cemetery<br>833 Jamaica Avenue<br>Brooklyn, NY 11208', 'seancewp')); ?></p>
             <!-- /wp:paragraph -->
             <!-- wp:spacer {"height":8} -->
             <div style="height:8px" aria-hidden="true" class="wp-block-spacer"></div>
@@ -71,7 +71,7 @@
             <!-- wp:buttons -->
             <div class="wp-block-buttons">
                 <!-- wp:button {"backgroundColor":"foreground"} -->
-                <div class="wp-block-button"><a class="wp-block-button__link has-foreground-background-color has-background"><?php echo wp_kses_post(__('Buy Tickets', 'seance')); ?></a></div>
+                <div class="wp-block-button"><a class="wp-block-button__link has-foreground-background-color has-background"><?php echo wp_kses_post(__('Buy Tickets', 'seancewp')); ?></a></div>
                 <!-- /wp:button -->
             </div>
             <!-- /wp:buttons -->
