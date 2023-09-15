a7e1239e1fcf6c662b4aff870bb1ad2e1b17af11
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-15 14:11:35 -0500

    Update footer-light-social-copyright.php

--- patterns/footer-light-social-copyright.php
+++ patterns/footer-light-social-copyright.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Footer light with social icons, and copyright
- * Slug: seancewp/footer-light-social-copyright
+ * Slug: seance/footer-light-social-copyright
  * Categories: footer
  * Description: Footer with social icons and copyright on a light block pattern
  * Keywords: footer, social, icons, copyright
@@ -10,7 +10,7 @@
  * Post Types: wp_template
  * Inserter: true
  * 
- * @package seancewp
+ * @package seance
  * @since 1.0.0
  */
 ?>
@@ -21,19 +21,18 @@
     <div class="wp-block-group alignwide" style="padding-top:4rem;padding-bottom:4rem">
         <!-- wp:social-links {"iconColor":"foreground","iconColorValue":"var(--wp--preset--color--foreground)","iconBackgroundColor":"background","iconBackgroundColorValue":"var(--wp--preset--color--background)","layout":{"type":"flex","justifyContent":"center"}} -->
         <ul class="wp-block-social-links has-icon-color has-icon-background-color">
-            <!-- wp:social-link {"url":"#","service":"facebook"} /-->
-            <!-- wp:social-link {"url":"#","service":"linkedin"} /-->
-            <!-- wp:social-link {"url":"#","service":"twitter"} /-->
-            <!-- wp:social-link {"url":"#","service":"instagram"} /-->
-            <!-- wp:social-link {"url":"#","service":"pinterest"} /-->
-            <!-- wp:social-link {"url":"#","service":"wordpress"} /-->
+            <!-- wp:social-link {"url":"facebook.com","service":"facebook","label":"Facebook"} /-->
+            <!-- wp:social-link {"url":"linkedin.com","service":"linkedin",,"label":"LinkedIn"} /-->
+            <!-- wp:social-link {"url":"instagram.com","service":"instagram","label":"Instagram"} /-->
+            <!-- wp:social-link {"url":"pinterest.com","service":"pinterest","label":"Pinterest"} /-->
+            <!-- wp:social-link {"url":"wordpress.org","service":"wordpress","label":"WordPress"} /-->
         </ul>
         <!-- /wp:social-links -->
         <!-- wp:spacer {"height":50} -->
         <div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
         <!-- /wp:spacer -->
         <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"16px"}}} -->
-        <p class="has-text-align-center" style="font-size:16px"><?php echo esc_html__('© Site Title', 'seancewp'); ?></p>
+        <p class="has-text-align-center" style="font-size:16px"><?php echo esc_html__('© Site Title', 'seance'); ?></p>
         <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
