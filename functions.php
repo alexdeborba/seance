5f528d7407f32ac4665341a17852572a6df7fb9f
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-15 15:43:15 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,14 +4,14 @@
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package seancewp
+ * @package seance
  * @author  Alex de Borba
  * @license GNU General Public License v2 or later
- * @link    https://github.com/alexdeborba/seancewp/
+ * @link    https://github.com/alexdeborba/seance/
  * @since   1.0.0
  */
 
-if ( ! function_exists( 'seancewp_support' ) ) :
+if ( ! function_exists( 'seance_support' ) ) :
 
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
@@ -20,7 +20,7 @@ if ( ! function_exists( 'seancewp_support' ) ) :
 	 *
 	 * @return void
 	 */
-	function seancewp_support() {
+	function seance_support() {
 
 		// Adds support for block styles.
 		add_theme_support( 'wp-block-styles' );
@@ -35,9 +35,9 @@ if ( ! function_exists( 'seancewp_support' ) ) :
 
 endif;
 
-add_action( 'after_setup_theme', 'seancewp_support' );
+add_action( 'after_setup_theme', 'seance_support' );
 
-if ( ! function_exists( 'seancewp_styles' ) ) :
+if ( ! function_exists( 'seance_styles' ) ) :
 
 	/**
 	 * Enqueue theme styles.
@@ -46,45 +46,45 @@ if ( ! function_exists( 'seancewp_styles' ) ) :
 	 *
 	 * @return void
 	 */
-	function seancewp_styles() {
+	function seance_styles() {
 		// Register theme stylesheet.
 		$theme_version = wp_get_theme()->get( 'Version' );
 
 		$version_string = is_string( $theme_version ) ? $theme_version : false;
 		wp_register_style(
-			'seancewp-style',
+			'seance-style',
 			get_template_directory_uri() . '/style.css',
 			array(),
 			$version_string
 		);
 
 		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'seancewp-style' );
+		wp_enqueue_style( 'seance-style' );
 
 	}
 
 endif;
 
-add_action( 'wp_enqueue_scripts', 'seancewp_styles' );
+add_action( 'wp_enqueue_scripts', 'seance_styles' );
 
 // Register pattern categories.
 function pattern_categories() {
 
 	$block_pattern_categories = array(
 		'post'    => array(
-			'label' => __( 'Post', 'seancewp' ),
+			'label' => __( 'Post', 'seance' ),
 		),
 		'pages'   => array(
-			'label' => __( 'Pages', 'seancewp' ),
+			'label' => __( 'Pages', 'seance' ),
 		),
 		'gallery' => array(
-			'label' => __( 'Gallery', 'seancewp' ),
+			'label' => __( 'Gallery', 'seance' ),
 		),
 		'query'   => array(
-			'label' => __( 'Query', 'seancewp' ),
+			'label' => __( 'Query', 'seance' ),
 		),
 		'footer'  => array(
-			'label' => __( 'Footer', 'seancewp' ),
+			'label' => __( 'Footer', 'seance' ),
 		),
 	);
 
@@ -95,7 +95,7 @@ function pattern_categories() {
 add_action( 'init', __NAMESPACE__ . '\pattern_categories', 9 );
 
 // Prevent loading patterns from the WordPress.org pattern directory.
-function seancewp_prevent_remote_patterns() {
+function seance_prevent_remote_patterns() {
 	return false;
 }
-add_filter( 'should_load_remote_block_patterns', 'seancewp_prevent_remote_patterns' );
+add_filter( 'should_load_remote_block_patterns', 'seance_prevent_remote_patterns' );
