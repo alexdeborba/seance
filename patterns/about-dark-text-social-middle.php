11b37dd2d5c09b16f553e8d9d70a7c31b52e1951
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-15 10:16:54 -0500

    Update about-dark-text-social-middle.php

--- patterns/about-dark-text-social-middle.php
+++ patterns/about-dark-text-social-middle.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: About dark with heading, paragraph, and social icons on the middle
- * Slug: seancewp/about-dark-text-social-middle
+ * Slug: seance/about-dark-text-social-middle
  * Categories: pages
  * Description: About with text on the middle on a dark block pattern
  * Keywords: heading, paragraph, social
@@ -10,7 +10,7 @@
  * Post Types:
  * Inserter: true
  * 
- * @package seancewp
+ * @package seance
  * @since 1.0.0
  */
 ?>
@@ -26,25 +26,24 @@
                 <div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
                 <!-- wp:heading {"style":{"typography":{"lineHeight":"1","textTransform":"uppercase","fontSize":"clamp(2.75rem, 6vw, 3.25rem)"}}} -->
-                <h2 class="wp-block-heading" id="margaret-fox" style="font-size:clamp(2.75rem, 6vw, 3.25rem);line-height:1;text-transform:uppercase"><?php echo wp_kses_post(__('Margaret<br>Fox', 'seancewp')); ?></h2>
+                <h2 class="wp-block-heading" id="margaret-fox" style="font-size:clamp(2.75rem, 6vw, 3.25rem);line-height:1;text-transform:uppercase"><?php echo wp_kses_post(__('Margaret<br>Fox', 'seance')); ?></h2>
                 <!-- /wp:heading -->
                 <!-- wp:spacer {"height":"8px"} -->
                 <div style="height:8px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
                 <!-- wp:paragraph {"fontSize":"small"} -->
-                <p class="has-small-font-size"><?php echo esc_html_e('I am Margaret Fox, one of the pioneering sisters from the dawn of spiritualism. With my unique gift, I bridged the gap between the living and the departed, revealing mysteries from the other side.', 'seancewp'); ?></p>
+                <p class="has-small-font-size"><?php echo esc_html_e('I am Margaret Fox, one of the pioneering sisters from the dawn of spiritualism. With my unique gift, I bridged the gap between the living and the departed, revealing mysteries from the other side.', 'seance'); ?></p>
                 <!-- /wp:paragraph -->
                 <!-- wp:spacer {"height":"8px"} -->
                 <div style="height:8px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
                 <!-- wp:social-links {"iconColor":"primary","iconColorValue":"var(--wp--preset--color--foreground)","className":"is-style-logos-only"} -->
                 <ul class="wp-block-social-links has-icon-color is-style-logos-only">
-                    <!-- wp:social-link {"url":"#","service":"facebook"} /-->
-                    <!-- wp:social-link {"url":"#","service":"linkedin"} /-->
-                    <!-- wp:social-link {"url":"#","service":"twitter"} /-->
-                    <!-- wp:social-link {"url":"#","service":"instagram"} /-->
-                    <!-- wp:social-link {"url":"#","service":"pinterest"} /-->
-                    <!-- wp:social-link {"url":"#","service":"wordpress"} /-->
+                <!-- wp:social-link {"url":"facebook.com","service":"facebook","label":"Facebook"} /-->
+                <!-- wp:social-link {"url":"linkedin.com","service":"linkedin",,"label":"LinkedIn"} /-->
+                <!-- wp:social-link {"url":"instagram.com","service":"instagram","label":"Instagram"} /-->
+                <!-- wp:social-link {"url":"pinterest.com","service":"pinterest","label":"Pinterest"} /-->
+                <!-- wp:social-link {"url":"wordpress.org","service":"wordpress","label":"WordPress"} /-->
                 </ul>
                 <!-- /wp:social-links -->
                 <!-- wp:spacer {"height":"64px"} -->
