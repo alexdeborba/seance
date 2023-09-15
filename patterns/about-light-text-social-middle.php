407147be228d7a7b87b22172aff606cad8a7100a
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-15 10:30:21 -0500

    Update about-light-text-social-middle.php

--- patterns/about-light-text-social-middle.php
+++ patterns/about-light-text-social-middle.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: About light with heading, paragraph, and social icons on the middle
- * Slug: seancewp/about-light-text-social-middle
+ * Slug: seance/about-light-text-social-middle
  * Categories: pages
  * Description: About with text on the middle on a light block pattern
  * Keywords: heading, paragraph, social
@@ -10,7 +10,7 @@
  * Post Types:
  * Inserter: true
  * 
- * @package seancewp
+ * @package seance
  * @since 1.0.0
  */
 ?>
@@ -25,25 +25,24 @@
                 <!-- wp:spacer {"height":64} -->
                 <div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer --><!-- wp:heading {"style":{"typography":{"lineHeight":"1","textTransform":"uppercase","fontSize":"clamp(2.75rem, 6vw, 3.25rem)"}}} -->
-                <h2 id="catherine-fox" style="font-size:clamp(2.75rem, 6vw, 3.25rem);line-height:1;text-transform:uppercase"><?php echo wp_kses_post(__('Catherine<br>Fox', 'seancewp')); ?></h2>
+                <h2 id="catherine-fox" style="font-size:clamp(2.75rem, 6vw, 3.25rem);line-height:1;text-transform:uppercase"><?php echo wp_kses_post(__('Catherine<br>Fox', 'seance')); ?></h2>
                 <!-- /wp:heading -->
                 <!-- wp:spacer {"height":8} -->
                 <div style="height:8px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
                 <!-- wp:paragraph {"fontSize":"small"} -->
-                <p class="has-small-font-size"><?php echo esc_html_e('I am Catherine Fox, the younger of the Fox siblings, and together with my sister Margaret, we ignited the spiritualist movement. With our shared gift, we unveiled the voices of the spirit realm to a world eager to listen.', 'seancewp'); ?></p>
+                <p class="has-small-font-size"><?php echo esc_html_e('I am Catherine Fox, the younger of the Fox siblings, and together with my sister Margaret, we ignited the spiritualist movement. With our shared gift, we unveiled the voices of the spirit realm to a world eager to listen.', 'seance'); ?></p>
                 <!-- /wp:paragraph -->
                 <!-- wp:spacer {"height":8} -->
                 <div style="height:8px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
                 <!-- wp:social-links {"iconColor":"foreground","iconColorValue":"var(--wp--preset--color--foreground)","className":"is-style-logos-only"} -->
                 <ul class="wp-block-social-links has-icon-color is-style-logos-only">
-                    <!-- wp:social-link {"url":"#","service":"facebook"} /-->
-                    <!-- wp:social-link {"url":"#","service":"linkedin"} /-->
-                    <!-- wp:social-link {"url":"#","service":"twitter"} /-->
-                    <!-- wp:social-link {"url":"#","service":"instagram"} /-->
-                    <!-- wp:social-link {"url":"#","service":"pinterest"} /-->
-                    <!-- wp:social-link {"url":"#","service":"wordpress"} /-->
+                    <!-- wp:social-link {"url":"facebook.com","service":"facebook","label":"Facebook"} /-->
+                    <!-- wp:social-link {"url":"linkedin.com","service":"linkedin",,"label":"LinkedIn"} /-->
+                    <!-- wp:social-link {"url":"instagram.com","service":"instagram","label":"Instagram"} /-->
+                    <!-- wp:social-link {"url":"pinterest.com","service":"pinterest","label":"Pinterest"} /-->
+                    <!-- wp:social-link {"url":"wordpress.org","service":"wordpress","label":"WordPress"} /-->
                 </ul>
                 <!-- /wp:social-links --><!-- wp:spacer {"height":64} -->
                 <div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
