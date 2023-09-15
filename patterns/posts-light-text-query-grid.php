0fdd9172381ff653422c8ef211212b7c5ec7c549
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-15 15:06:13 -0500

    Update posts-light-text-query-grid.php

--- patterns/posts-light-text-query-grid.php
+++ patterns/posts-light-text-query-grid.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Posts light grid with title, excerpt, and date
- * Slug: seancewp/posts-light-text-query-grid
+ * Slug: seance/posts-light-text-query-grid
  * Categories: query, posts, text, grid
  * Description: Posts text grid on a light block pattern
  * Keywords: text, blog, posts, grid
@@ -10,7 +10,7 @@
  * Post Types:
  * Inserter: true
  *
- * @package seancewp
+ * @package seance
  * @since 1.0.0
  */
 ?>
