037f347e6def5b54cd195a7a0b4aee9d4a355ae9
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-15 16:44:02 -0500

    Remove queryId
    Removed queryId as per Pattern Creation Guideline

--- patterns/posts-light-media-query-grid.php
+++ patterns/posts-light-media-query-grid.php
@@ -18,7 +18,7 @@
 
 <!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide">
-    <!-- wp:query {"queryId":43,"query":{"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","sticky":"","perPage":12},"align":"wide","layout":{"contentSize":null,"type":"constrained"}} -->
+    <!-- wp:query {"query":{"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","sticky":"","perPage":12},"align":"wide","layout":{"contentSize":null,"type":"constrained"}} -->
     <div class="wp-block-query alignwide">
         <!-- wp:post-template {"align":"wide","layout":{"type":"grid","columnCount":3}} -->
         <!-- wp:post-featured-image {"isLink":true,"width":"100%","height":"318px"} /-->
