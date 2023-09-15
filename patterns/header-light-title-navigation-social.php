852d76f6e2bb0a9f638448b758d821c2b4a54f62
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-15 14:34:55 -0500

    Update header-light-title-navigation-social.php

--- patterns/header-light-title-navigation-social.php
+++ patterns/header-light-title-navigation-social.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Header with site title, navigation, and social icons on light background
- * Slug: seancewp/header-light-title-navigation-social
+ * Slug: seance/header-light-title-navigation-social
  * Categories: header
  * Description: Header with site title, navigation, and social icons on light background block pattern
  * Keywords: header, navigation, social
@@ -10,7 +10,7 @@
  * Post Types: wp_template
  * Inserter: true
  * 
- * @package seancewp
+ * @package seance
  * @since 1.0.0
  */
 ?>
@@ -20,16 +20,27 @@
     <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var(--wp--custom--spacing--large, 8rem)","top":"var(--wp--custom--spacing--small, 1.25rem)"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
     <div class="wp-block-group alignwide" style="padding-top:var(--wp--custom--spacing--small, 1.25rem);padding-bottom:var(--wp--custom--spacing--large, 8rem)">
         <!-- wp:site-title {"style":{"typography":{"fontStyle":"italic","fontWeight":"400"}}} /-->
-        <!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} -->
+        <!-- wp:navigation {"icon":"menu","overlayBackgroundColor":"foreground","overlayTextColor":"primary","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"layout":{"selfStretch":"fit","flexSize":null},"typography":{"fontStyle":"italic","fontWeight":"400"}},"fontSize":"medium","fontFamily":"source-serif-pro"} -->
+        <!-- wp:navigation-link {"label":"Home","url":"#","kind":"custom","isTopLevelLink":true} /-->
+        <!-- wp:navigation-link {"label":"About","url":"#","kind":"custom","isTopLevelLink":true} /-->
+        <!-- wp:navigation-submenu {"label":"Events","url":"#","kind":"custom","isTopLevelItem":true} -->
+        <!-- wp:navigation-link {"label":"This Week","description":"All events taken place this week","url":"#","kind":"custom","isTopLevelLink":false} /-->
+        <!-- wp:navigation-link {"label":"This Month","description":"All events taken place this month","url":"#","kind":"custom","isTopLevelLink":false} /-->
+        <!-- wp:navigation-link {"label":"This Year","description":"All events taken place this year","url":"#","kind":"custom","isTopLevelLink":false} /-->
+        <!-- /wp:navigation-submenu -->
+        <!-- wp:navigation-link {"label":"Blog","url":"#","kind":"custom","isTopLevelLink":true} /-->
+        <!-- wp:navigation-link {"label":"Gallery","url":"#","kind":"custom","isTopLevelLink":true} /-->
+        <!-- wp:navigation-link {"label":"Contact","url":"#","kind":"custom","isTopLevelLink":true} /-->
+        <!-- /wp:navigation -->
         <!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
+        <!-- /wp:navigation -->
         <!-- wp:social-links {"iconColor":"foreground","iconColorValue":"var(--wp--preset--color--foreground)","className":"is-style-logos-only"} -->
         <ul class="wp-block-social-links has-icon-color is-style-logos-only">
-            <!-- wp:social-link {"url":"#","service":"facebook"} /-->
-            <!-- wp:social-link {"url":"#","service":"linkedin"} /-->
-            <!-- wp:social-link {"url":"#","service":"twitter"} /-->
-            <!-- wp:social-link {"url":"#","service":"instagram"} /-->
-            <!-- wp:social-link {"url":"#","service":"pinterest"} /-->
-            <!-- wp:social-link {"url":"#","service":"wordpress"} /-->
+            <!-- wp:social-link {"url":"facebook.com","service":"facebook","label":"Facebook"} /-->
+            <!-- wp:social-link {"url":"linkedin.com","service":"linkedin",,"label":"LinkedIn"} /-->
+            <!-- wp:social-link {"url":"instagram.com","service":"instagram","label":"Instagram"} /-->
+            <!-- wp:social-link {"url":"pinterest.com","service":"pinterest","label":"Pinterest"} /-->
+            <!-- wp:social-link {"url":"wordpress.org","service":"wordpress","label":"WordPress"} /-->
         </ul>
         <!-- /wp:social-links -->
         <!-- /wp:navigation -->
