e12cb8a6944ff44a6e7e11c5e887dc8fafbd5e71
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-18 05:35:44 -0500

    Update about-light-text-social-middle.php

--- patterns/about-light-text-social-middle.php
+++ patterns/about-light-text-social-middle.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Title: About light with heading, paragraph, and social icons on the middle
+ * Title: About - Light (Text and Social Links Centered)
  * Slug: seance/about-light-text-social-middle
- * Categories: pages
- * Description: About with text on the middle on a light block pattern
+ * Categories: about, pages
+ * Description: A centered light block pattern featuring a title, a brief description, and social media icons.
  * Keywords: heading, paragraph, social
  * Viewport Width: 1500
- * Block Types: core/pages
- * Post Types:
+ * Block Types: core/group, core/cover, core/heading, core/paragraph, core/social-links, core/social-link
+ * Post Types: post, page
  * Inserter: true
  * 
  * @package seance
@@ -18,13 +18,15 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"1.25rem","right":"1.25rem","bottom":"1.25rem","left":"1.25rem"}}}} -->
 <div class="wp-block-group alignfull" style="padding-top:1.25rem;padding-right:1.25rem;padding-bottom:1.25rem;padding-left:1.25rem">
     <!-- wp:cover {"overlayColor":"primary","minHeight":80,"minHeightUnit":"vh","isDark":false,"align":"full"} -->
-    <div class="wp-block-cover alignfull is-light" style="min-height:80vh"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span>
+    <div class="wp-block-cover alignfull is-light" style="min-height:80vh">
+    <span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span>
         <div class="wp-block-cover__inner-container">
             <!-- wp:group {"layout":{"inherit":false,"contentSize":"400px","type":"constrained"}} -->
             <div class="wp-block-group">
                 <!-- wp:spacer {"height":64} -->
                 <div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
-                <!-- /wp:spacer --><!-- wp:heading {"style":{"typography":{"lineHeight":"1","textTransform":"uppercase","fontSize":"clamp(2.75rem, 6vw, 3.25rem)"}}} -->
+                <!-- /wp:spacer -->
+                <!-- wp:heading {"style":{"typography":{"lineHeight":"1","textTransform":"uppercase","fontSize":"clamp(2.75rem, 6vw, 3.25rem)"}}} -->
                 <h2 id="catherine-fox" style="font-size:clamp(2.75rem, 6vw, 3.25rem);line-height:1;text-transform:uppercase"><?php echo wp_kses_post(__('Catherine<br>Fox', 'seance')); ?></h2>
                 <!-- /wp:heading -->
                 <!-- wp:spacer {"height":8} -->
@@ -36,15 +38,16 @@
                 <!-- wp:spacer {"height":8} -->
                 <div style="height:8px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
-                <!-- wp:social-links {"iconColor":"foreground","iconColorValue":"var(--wp--preset--color--foreground)","className":"is-style-logos-only"} -->
-                <ul class="wp-block-social-links has-icon-color is-style-logos-only">
+                <!-- wp:social-links {"iconColor":"background","iconColorValue":"var(--wp--preset--color--background)","iconBackgroundColor":"foreground","iconBackgroundColorValue":"var(--wp--preset--color--foreground)","openInNewTab":true} -->
+                <ul class="wp-block-social-links has-icon-color has-icon-background-color">
                     <!-- wp:social-link {"url":"facebook.com","service":"facebook","label":"Facebook"} /-->
-                    <!-- wp:social-link {"url":"linkedin.com","service":"linkedin",,"label":"LinkedIn"} /-->
+                    <!-- wp:social-link {"url":"linkedin.com","service":"linkedin","label":"LinkedIn"} /-->
                     <!-- wp:social-link {"url":"instagram.com","service":"instagram","label":"Instagram"} /-->
                     <!-- wp:social-link {"url":"pinterest.com","service":"pinterest","label":"Pinterest"} /-->
                     <!-- wp:social-link {"url":"wordpress.org","service":"wordpress","label":"WordPress"} /-->
                 </ul>
-                <!-- /wp:social-links --><!-- wp:spacer {"height":64} -->
+                <!-- /wp:social-links -->
+                <!-- wp:spacer {"height":64} -->
                 <div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
             </div>
