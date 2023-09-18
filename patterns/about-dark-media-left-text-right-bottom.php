a1dbf7d3755d5c53b0d4e699fcda67ad6401c707
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-18 04:48:47 -0500

    Update about-dark-media-left-text-right-bottom.php

--- patterns/about-dark-media-left-text-right-bottom.php
+++ patterns/about-dark-media-left-text-right-bottom.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * Title: About dark with media on the left, and heading, paragraph, social icons on bottom right
+ * Title: About - Dark (Media on Left, Heading, Paragraph, and Social Icons Bottom Right)
  * Slug: seance/about-dark-media-left-text-right-bottom
- * Categories: pages
- * Description: About with media on the left and text on the right botton on a dark block pattern
- * Keywords: image, heading, paragraph, social
+ * Categories: about, dark, image, paragraph, social
+ * Description: A dark block pattern for an About section featuring media on the left and text bottom on the right.
+ * Keywords: about, dark, heading, image, paragraph, social
  * Viewport Width: 1500
- * Block Types: core/pages
- * Post Types:
+ * Block Types: core/group, core/media-text, core/spacer, core/heading, core/paragraph, core/social-links
  * Inserter: true
  * 
  * @package seance
@@ -19,7 +18,7 @@
 <div class="wp-block-group alignfull">
     <!-- wp:media-text {"align":"full","mediaType":"image","imageFill":true,"focalPoint":{"x":"0.50","y":"0.10"},"backgroundColor":"foreground","className":"alignfull is-image-fill has-background-color has-text-color has-background has-link-color"} -->
     <div class="wp-block-media-text alignfull is-stacked-on-mobile is-image-fill has-background-color has-text-color has-background has-link-color has-foreground-background-color has-background">
-        <figure class="wp-block-media-text__media" style="background-image:url(<?php echo esc_url(get_theme_file_uri("/assets/images/about-dark-left.jpg")); ?>);background-position:50% 10%"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/about-dark-left.jpg")); ?>" alt="<?php echo esc_html_e('The Fox Sisters', 'seance'); ?>" /></figure>
+        <figure class="wp-block-media-text__media" style="background-image:url(<?php echo esc_url(get_theme_file_uri("/assets/images/about-dark-left.jpg")); ?>);background-position:50% 10%"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/about-dark-left.jpg")); ?>" alt="<?php echo esc_html_e('Image Representation of The Fox Sisters', 'seance'); ?>" /></figure>
         <div class="wp-block-media-text__content">
             <!-- wp:spacer {"height":32} -->
             <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
@@ -27,7 +26,7 @@
             <!-- wp:group {"style":{"spacing":{"padding":{"right":"min(8rem, 5vw)","top":"min(28rem, 28vw)"}}}} -->
             <div class="wp-block-group" style="padding-top:min(28rem, 28vw);padding-right:min(8rem, 5vw)">
                 <!-- wp:heading {"style":{"typography":{"fontWeight":"300","lineHeight":"1.115","fontSize":"clamp(3rem, 6vw, 4.5rem)"}}} -->
-                <h2 style="font-size:clamp(3rem, 6vw, 4.5rem);font-weight:300;line-height:1.115"><em><?php echo wp_kses_post(__('The Fox <br> Sisters', 'seance')); ?></em></h2>
+                <h2 style="font-size:clamp(3rem, 6vw, 4.5rem);font-weight:300;line-height:1.115"><em><?php echo wp_kses_post(__('The Fox<br>Sisters', 'seance')); ?></em></h2>
                 <!-- /wp:heading -->
                 <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6"}}} -->
                 <p style="line-height:1.6"><?php echo esc_html_e('We are Margaret, Catherine, and Leah Fox, renowned spiritual mediums from a bygone era, now embracing the digital age. With our unique abilities, we have connected countless souls with the messages of the spirit world.', 'seance'); ?></p>
@@ -35,10 +34,10 @@
                 <!-- wp:spacer {"height":40} -->
                 <div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
-                <!-- wp:social-links {"iconColor":"background","iconColorValue":"var(--wp--preset--color--background)","iconBackgroundColor":"foreground","iconBackgroundColorValue":"var(--wp--preset--color--foreground)"} -->
+                <!-- wp:social-links {"iconColor":"background","iconColorValue":"var(--wp--preset--color--background)","iconBackgroundColor":"foreground","iconBackgroundColorValue":"var(--wp--preset--color--foreground)","openInNewTab":true} -->
                 <ul class="wp-block-social-links has-icon-color has-icon-background-color">
                     <!-- wp:social-link {"url":"facebook.com","service":"facebook","label":"Facebook"} /-->
-                    <!-- wp:social-link {"url":"linkedin.com","service":"linkedin",,"label":"LinkedIn"} /-->
+                    <!-- wp:social-link {"url":"linkedin.com","service":"linkedin","label":"LinkedIn"} /-->
                     <!-- wp:social-link {"url":"instagram.com","service":"instagram","label":"Instagram"} /-->
                     <!-- wp:social-link {"url":"pinterest.com","service":"pinterest","label":"Pinterest"} /-->
                     <!-- wp:social-link {"url":"wordpress.org","service":"wordpress","label":"WordPress"} /-->
