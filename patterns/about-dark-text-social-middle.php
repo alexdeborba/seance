34b3a5d85f3e711f0cf59c493dca85521da7a4e7
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-18 05:09:51 -0500

    Update about-dark-text-social-middle.php

--- patterns/about-dark-text-social-middle.php
+++ patterns/about-dark-text-social-middle.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * Title: About dark with heading, paragraph, and social icons on the middle
+ * Title: About - Dark (Heading, Paragraph, and Social Icons Centered Middle)
  * Slug: seance/about-dark-text-social-middle
- * Categories: pages
- * Description: About with text on the middle on a dark block pattern
- * Keywords: heading, paragraph, social
+ * Categories: about, dark, heading, paragraph, social
+ * Description: A dark block pattern for an about section featuring text and social icons centered in the middle.
+ * Keywords: dark, heading, paragraph, social
  * Viewport Width: 1500
- * Block Types: core/pages
- * Post Types:
+ * Block Types: core/group, core/cover, core/heading, core/paragraph, core/social-links, core/spacer
  * Inserter: true
  * 
  * @package seance
@@ -18,7 +17,8 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"1.25rem","right":"1.25rem","bottom":"1.25rem","left":"1.25rem"}}},"backgroundColor":"foreground"} -->
 <div class="wp-block-group alignfull has-foreground-background-color has-background" style="padding-top:1.25rem;padding-right:1.25rem;padding-bottom:1.25rem;padding-left:1.25rem">
     <!-- wp:cover {"dimRatio":0,"minHeight":80,"minHeightUnit":"vh","isDark":false,"align":"full","textColor":"primary"} -->
-    <div class="wp-block-cover alignfull is-light has-primary-color has-text-color" style="min-height:80vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+    <div class="wp-block-cover alignfull is-light has-primary-color has-text-color" style="min-height:80vh">
+        <span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
         <div class="wp-block-cover__inner-container">
             <!-- wp:group {"layout":{"inherit":false,"contentSize":"400px","type":"constrained"}} -->
             <div class="wp-block-group">
@@ -37,13 +37,13 @@
                 <!-- wp:spacer {"height":"8px"} -->
                 <div style="height:8px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
-                <!-- wp:social-links {"iconColor":"primary","iconColorValue":"var(--wp--preset--color--foreground)","className":"is-style-logos-only"} -->
-                <ul class="wp-block-social-links has-icon-color is-style-logos-only">
-                <!-- wp:social-link {"url":"facebook.com","service":"facebook","label":"Facebook"} /-->
-                <!-- wp:social-link {"url":"linkedin.com","service":"linkedin",,"label":"LinkedIn"} /-->
-                <!-- wp:social-link {"url":"instagram.com","service":"instagram","label":"Instagram"} /-->
-                <!-- wp:social-link {"url":"pinterest.com","service":"pinterest","label":"Pinterest"} /-->
-                <!-- wp:social-link {"url":"wordpress.org","service":"wordpress","label":"WordPress"} /-->
+                <!-- wp:social-links {"iconColor":"background","iconColorValue":"var(--wp--preset--color--background)","iconBackgroundColor":"foreground","iconBackgroundColorValue":"var(--wp--preset--color--foreground)","openInNewTab":true} -->
+                <ul class="wp-block-social-links has-icon-color has-icon-background-color">
+                    <!-- wp:social-link {"url":"facebook.com","service":"facebook","label":"Facebook"} /-->
+                    <!-- wp:social-link {"url":"linkedin.com","service":"linkedin","label":"LinkedIn"} /-->
+                    <!-- wp:social-link {"url":"instagram.com","service":"instagram","label":"Instagram"} /-->
+                    <!-- wp:social-link {"url":"pinterest.com","service":"pinterest","label":"Pinterest"} /-->
+                    <!-- wp:social-link {"url":"wordpress.org","service":"wordpress","label":"WordPress"} /-->
                 </ul>
                 <!-- /wp:social-links -->
                 <!-- wp:spacer {"height":"64px"} -->
