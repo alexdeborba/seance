5921526b46259551496423663102c496987cf9d5
Atmostfear Entertainment <mail@alexdeborba.com>
2023-09-18 04:58:55 -0500

    Update about-dark-media-right-text-left-bottom.php

--- patterns/about-dark-media-right-text-left-bottom.php
+++ patterns/about-dark-media-right-text-left-bottom.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * Title: About dark with media on the right, and heading, paragraph, social icons on bottom left
+ * Title: About - Dark (Media on Right, Heading, Paragraph, and Social Icons on Bottom Left)
  * Slug: seance/about-dark-media-right-text-left-bottom
- * Categories: pages
- * Description: About with media on the right and text on the bottom left on a dark block pattern
- * Keywords: image, heading, paragraph, social
+ * Categories: about, dark, heading, image, paragraph, social
+ * Description: A dark block pattern for an about section featuring media on the right and text at the bottom left.
+ * Keywords: about, dark, heading, image, paragraph, social
  * Viewport Width: 1500
- * Block Types: core/pages
- * Post Types:
+ * Block Types: core/group, core/media-text, core/spacer, core/heading, core/paragraph, core/social-links
  * Inserter: true
  * 
  * @package seance
@@ -26,7 +25,7 @@
             <!-- wp:group {"style":{"spacing":{"padding":{"right":"min(8rem, 5vw)","top":"min(28rem, 28vw)"}}}} -->
             <div class="wp-block-group" style="padding-top:min(28rem, 28vw);padding-right:min(8rem, 5vw)">
                 <!-- wp:heading {"style":{"typography":{"fontWeight":"300","lineHeight":"1.115","fontSize":"clamp(3rem, 6vw, 4.5rem)"}}} -->
-                <h2 class="wp-block-heading" style="font-size:clamp(3rem, 6vw, 4.5rem);font-weight:300;line-height:1.115"><em><?php echo wp_kses_post(__('The Fox <br> Sisters', 'seance')); ?></em></h2>
+                <h2 class="wp-block-heading" style="font-size:clamp(3rem, 6vw, 4.5rem);font-weight:300;line-height:1.115"><em><?php echo wp_kses_post(__('The Fox<br>Sisters', 'seance')); ?></em></h2>
                 <!-- /wp:heading -->
                 <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6"}}} -->
                 <p style="line-height:1.6"><?php echo esc_html_e('We are Margaret, Catherine, and Leah Fox, renowned spiritual mediums from a bygone era, now embracing the digital age. With our unique abilities, we have connected countless souls with the messages of the spirit world.', 'seance'); ?></p>
@@ -34,10 +33,10 @@
                 <!-- wp:spacer {"height":"40px"} -->
                 <div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
                 <!-- /wp:spacer -->
-                <!-- wp:social-links {"iconColor":"background","iconColorValue":"var(--wp--preset--color--background)","iconBackgroundColor":"foreground","iconBackgroundColorValue":"var(--wp--preset--color--foreground)"} -->
+                <!-- wp:social-links {"iconColor":"background","iconColorValue":"var(--wp--preset--color--background)","iconBackgroundColor":"foreground","iconBackgroundColorValue":"var(--wp--preset--color--foreground)","openInNewTab":true} -->
                 <ul class="wp-block-social-links has-icon-color has-icon-background-color">
                     <!-- wp:social-link {"url":"facebook.com","service":"facebook","label":"Facebook"} /-->
-                    <!-- wp:social-link {"url":"linkedin.com","service":"linkedin",,"label":"LinkedIn"} /-->
+                    <!-- wp:social-link {"url":"linkedin.com","service":"linkedin","label":"LinkedIn"} /-->
                     <!-- wp:social-link {"url":"instagram.com","service":"instagram","label":"Instagram"} /-->
                     <!-- wp:social-link {"url":"pinterest.com","service":"pinterest","label":"Pinterest"} /-->
                     <!-- wp:social-link {"url":"wordpress.org","service":"wordpress","label":"WordPress"} /-->
@@ -49,7 +48,7 @@
             <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->
         </div>
-        <figure class="wp-block-media-text__media" style="background-image:url(<?php echo esc_url(get_theme_file_uri("/assets/images/about-dark-right.jpg")); ?>);background-position:50% 10%"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/about-dark-right.jpg")); ?>" alt="<?php echo esc_html_e('The Fox Sisters', 'seance'); ?>" /></figure>
+        <figure class="wp-block-media-text__media" style="background-image:url(<?php echo esc_url(get_theme_file_uri("/assets/images/about-dark-right.jpg")); ?>);background-position:50% 10%"><img src="<?php echo esc_url(get_theme_file_uri("/assets/images/about-dark-right.jpg")); ?>" alt="<?php echo esc_html_e('Image Representation of The Fox Sisters', 'seance'); ?>" /></figure>
     </div>
     <!-- /wp:media-text -->
 </div>
